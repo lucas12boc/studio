59dc6373a3bc1597dc5d09228227291b11142b5f
lucas guzman <lucasyabi125@gmail.com>
2025-06-13 20:58:28 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/sidebar-nav.tsx
+++ src/components/sidebar-nav.tsx
@@ -48,16 +48,17 @@ export function SidebarNav() {
         <SidebarMenu>
           {navItems.map((item) => (
             <SidebarMenuItem key={item.href}>
-              <Link href={item.href} asChild>
-                <SidebarMenuButton
-                  isActive={pathname === item.href}
-                  tooltip={item.label}
-                  className="justify-start"
-                >
+              <SidebarMenuButton
+                asChild
+                isActive={pathname === item.href}
+                tooltip={item.label}
+                className="justify-start"
+              >
+                <Link href={item.href}>
                   <item.icon className="h-5 w-5" />
                   <span>{item.label}</span>
-                </SidebarMenuButton>
-              </Link>
+                </Link>
+              </SidebarMenuButton>
             </SidebarMenuItem>
           ))}
         </SidebarMenu>
